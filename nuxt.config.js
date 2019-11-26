68189ec23331687217dd0cd65735d69e692411d5
Texmeijin <i8858garden@gmail.com>
2019-11-26 09:53:27 +0900

    LPのラフ記述

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,38 +5,55 @@ export default {
   env,
   mode: 'spa',
   /*
-  ** Headers of the page
-  */
+   ** Headers of the page
+   */
   head: {
     title: process.env.npm_package_name || '',
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+    meta: [{
+        charset: 'utf-8'
+      },
+      {
+        name: 'viewport',
+        content: 'width=device-width, initial-scale=1'
+      },
+      {
+        hid: 'description',
+        name: 'description',
+        content: process.env.npm_package_description || ''
+      }
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+    link: [{
+        rel: 'icon',
+        type: 'image/x-icon',
+        href: '/favicon.ico'
+      },
+      {
+        rel: 'stylesheet',
+        href: 'https://fonts.googleapis.com/css?family=Noto+Sans+JP&display=swap'
+      }
+    ],
   },
   /*
-  ** Customize the progress-bar color
-  */
-  loading: { color: '#fff' },
+   ** Customize the progress-bar color
+   */
+  loading: {
+    color: '#fff'
+  },
   /*
-  ** Global CSS
-  */
+   ** Global CSS
+   */
   css: ['@/assets/css/main.scss'],
   /*
-  ** Plugins to load before mounting the App
-  */
+   ** Plugins to load before mounting the App
+   */
   plugins: ['@/plugins/firebase', '@/plugins/auth.ts'],
   /*
-  ** Nuxt.js dev-modules
-  */
+   ** Nuxt.js dev-modules
+   */
   buildModules: ['@nuxt/typescript-build'],
   /*
-  ** Nuxt.js modules
-  */
+   ** Nuxt.js modules
+   */
   modules: [
     '@nuxtjs/pwa',
     '@nuxtjs/style-resources'
@@ -47,13 +64,12 @@ export default {
     ]
   },
   /*
-  ** Build configuration
-  */
+   ** Build configuration
+   */
   build: {
     /*
-    ** You can extend webpack config here
-    */
-    extend (config, ctx) {
-    }
+     ** You can extend webpack config here
+     */
+    extend(config, ctx) {}
   }
 }
--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,30 @@
-<template lang="pug">
-.sec-page
-  h1 top page
-  p(v-if="isLogined")
-    span {{ user.displayName }}
-    button(@click="logout(uid)") ログアウト
-  p(v-else)
-    button(@click="login") ログイン
+<template>
+  <main>
+    <section class="heading">
+      <h1 class="heading-catch">
+        <span class="lg">
+          縁メモ
+          <span class="foot">-enmemo-</span>
+        </span>
+        <span class="md">
+          あなたの”縁”を、
+          <br>忘れないように。
+        </span>
+      </h1>
+    </section>
+    <section class="inquire">
+      <h2 class="inquire-message">
+        このTwitterアカウント、どこで出会った誰だっけ？
+      </h2>
+      <span class="inquire-statement">SNSで繋がって数ヶ月。久々に見かけたそのアカウントのこと、あなたは覚えていますか？</span>
+    </section>
+    <aside class="footer">
+      <a v-if="isLogined" class="footer-twitterLogin" href="/post">縁をメモする</a>
+      <button v-else class="footer-twitterLogin" @click="login()">
+        <span>Twitterでログイン</span>
+      </button>
+    </aside>
+  </main>
 </template>
 
 <script lang="ts">
@@ -29,3 +48,73 @@ export default class Index extends Vue {
   }
 }
 </script>
+
+<style scoped lang="scss">
+.heading {
+  &-catch {
+    background-image: url(~assets/img/heading/people-circle.jpg);
+    background-size: cover;
+    background-repeat: no-repeat;
+    background-position: center;
+    min-height: 300px;
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-end;
+    padding: 40px 24px;
+
+    .lg {
+      color: $white;
+      font-weight: bold;
+      font-size: 2.8rem;
+
+      .foot {
+        font-size: 1.4rem;
+      }
+    }
+
+    .md {
+      color: $white;
+      font-weight: bold;
+      font-size: 1.8rem;
+    }
+  }
+}
+
+.inquire {
+  padding: 40px 24px;
+
+  &-message {
+    font-weight: bold;
+    font-size: 1.8rem;
+  }
+
+  &-statement {
+    font-size: 1.2rem;
+  }
+}
+
+.footer {
+  padding: 8px;
+  position: fixed;
+  bottom: 0;
+  display: flex;
+  justify-content: center;
+  background: #33333333;
+  width: 100%;
+
+  &-twitterLogin {
+    width: 100%;
+    border-radius: 30px;
+    height: 60px;
+    box-sizing: border-box;
+    padding: 8px 16px;
+    color: $white;
+    background: #58a8e9;
+    font-size: 1.4rem;
+    font-weight: bold;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+}
+</style>
